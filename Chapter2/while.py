b2a30cd17b1133171b3c7077cf19bd92d1e57e88
JianTengYan <jianteng_yan@hotmail.com>
2021-11-17 00:29:06 -0800

    python

---
+++ Chapter2/while.py
@@ -0,0 +1,12 @@
+#simple while
+
+x_int = 0
+
+#test loop-control variable at beginning  of loop
+while x_int < 10:
+	print(x_int, end= ' ')	#
+	
+	x_int = x_int + 1	#change loop-control variable
+	
+print()
+print("Final value of x_int: ", x_int)	#bigger than value printed in loop !
